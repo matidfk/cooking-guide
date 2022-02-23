117ec647b6eb5fe48b3c6a20fc6e618b643cd0ab
matidfk <72168331+matidfk@users.noreply.github.com>
2022-02-23 17:51:02 +0000

    add /docs

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			base: dev ? '' : '/cooking-guide',
+		},
+		//target: '#svelte'
+	}
+};
+
+export default config;
